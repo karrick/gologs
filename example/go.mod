1f7361c9cf8f83fdabbcae438a2c3ebe2b5d3e72
Karrick S. McDermott <karrick@karrick.net>
2019-11-19 17:19:18 -0500

    log format for example

---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module github.com/karrick/gologs/example
+
+replace github.com/karrick/gologs => ../
+
+require github.com/karrick/gologs v0.0.2 // indirect
--- example/main.go
+++ example/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/karrick/gologs"
 )
 
 func main() {
-	base := gologs.New(os.Stderr, "[LOG] ")
+	var ProgramName string
+	var err error
+	if ProgramName, err = os.Executable(); err != nil {
+		ProgramName = os.Args[0]
+	}
+	ProgramName = filepath.Base(ProgramName)
+
+	base := gologs.New(os.Stderr, fmt.Sprintf("[%s] {message}", ProgramName))
 	base.User("%v %v %v", 3.14, "hello", struct{}{})
 }
