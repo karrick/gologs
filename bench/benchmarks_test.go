1cea380e8cfad766b817774c6edb096211777c47
Karrick S. McDermott <karrick@karrick.dev>
2022-08-11 04:43:50 -0400

    additional comparison benchmarks
    ```Bash
    go test -bench=. -benchmem
    goos: freebsd
    goarch: amd64
    pkg: github.com/karrick/gologs/bench
    cpu: AMD Ryzen Threadripper 3960X 24-Core Processor
    BenchmarkLogEmpty/gologs-48        7035302     172.8   ns/op  0  B/op  0  allocs/op
    BenchmarkLogEmpty/zerolog-48       301638574   3.406   ns/op  0  B/op  0  allocs/op
    BenchmarkLogDisabled/gologs-48     1000000000  0.1013  ns/op  0  B/op  0  allocs/op
    BenchmarkLogDisabled/zerolog-48    1000000000  0.1135  ns/op  0  B/op  0  allocs/op
    BenchmarkLogInfo/gologs-48         2788746     422.1   ns/op  0  B/op  0  allocs/op
    BenchmarkLogInfo/zerolog-48        172204484   6.962   ns/op  0  B/op  0  allocs/op
    BenchmarkContextFields/gologs-48   2311048     518.7   ns/op  0  B/op  0  allocs/op
    BenchmarkContextFields/zerolog-48  309643886   3.855   ns/op  0  B/op  0  allocs/op
    BenchmarkLogFields/gologs-48       1432671     836.2   ns/op  0  B/op  0  allocs/op
    BenchmarkLogFields/zerolog-48      70282065    16.88   ns/op  0  B/op  0  allocs/op
    PASS
    ok  	github.com/karrick/gologs/bench	13.288s
    ```

--- bench/benchmarks_test.go
+++ bench/benchmarks_test.go
@@ -9,43 +9,135 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// go test -bench=. -benchmem
-// goos: freebsd
-// goarch: amd64
-// pkg: github.com/karrick/gologs/bench
-// cpu: AMD Ryzen Threadripper 3960X 24-Core Processor
-// BenchmarkLogFieldsZerolog-48    70580732  17.92    ns/op  0  B/op  0  allocs/op
-// BenchmarkLogFieldsGologs-48   1000000000   0.1212  ns/op  0  B/op  0  allocs/op
-// PASS
-// ok  	github.com/karrick/gologs/bench	1.430s
-
 const fakeMessage = "Test logging, but use a somewhat realistic message length."
 
-func BenchmarkLogFieldsZerolog(b *testing.B) {
-	logger := zerolog.New(ioutil.Discard)
-	b.ResetTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			logger.Info().
-				Str("string", "four!").
-				Time("time", time.Time{}).
-				Int("int", 123).
-				Float32("float", -2.203230293249593).
-				Msg(fakeMessage)
-		}
+func BenchmarkLogEmpty(b *testing.B) {
+	b.Run("gologs", func(b *testing.B) {
+		logger := gologs.New(ioutil.Discard)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Log().Msg("")
+			}
+		})
+	})
+
+	b.Run("zerolog", func(b *testing.B) {
+		logger := zerolog.New(ioutil.Discard)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Log().Msg("")
+			}
+		})
 	})
 }
 
-func BenchmarkLogFieldsGologs(b *testing.B) {
-	logger := gologs.New(ioutil.Discard).SetTimeFormatter(gologs.TimeUnix)
-	b.ResetTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			logger.Info().
-				String("string", "four!").
-				Int("int", 123).
-				Float("float", -2.203230293249593).
-				Msg(fakeMessage)
-		}
+func BenchmarkLogDisabled(b *testing.B) {
+	b.Run("gologs", func(b *testing.B) {
+		logger := gologs.New(ioutil.Discard).SetError()
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().Msg(fakeMessage)
+			}
+		})
+	})
+
+	b.Run("zerolog", func(b *testing.B) {
+		logger := zerolog.New(ioutil.Discard).Level(zerolog.Disabled)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().Msg(fakeMessage)
+			}
+		})
+	})
+}
+
+func BenchmarkLogInfo(b *testing.B) {
+	b.Run("gologs", func(b *testing.B) {
+		logger := gologs.New(ioutil.Discard).SetInfo()
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().Msg(fakeMessage)
+			}
+		})
+	})
+
+	b.Run("zerolog", func(b *testing.B) {
+		logger := zerolog.New(ioutil.Discard)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().Msg(fakeMessage)
+			}
+		})
+	})
+}
+
+func BenchmarkContextFields(b *testing.B) {
+	b.Run("gologs", func(b *testing.B) {
+		logger := gologs.New(ioutil.Discard).SetInfo().
+			SetTimeFormatter(gologs.TimeUnix).
+			With().
+			String("string", "four!").
+			Int("int", 123).
+			Float("float", -2.203230293249593).
+			Logger()
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().Msg(fakeMessage)
+			}
+		})
+	})
+
+	b.Run("zerolog", func(b *testing.B) {
+		logger := zerolog.New(ioutil.Discard).
+			With().
+			Str("string", "four!").
+			Time("time", time.Time{}).
+			Int("int", 123).
+			Float32("float", -2.203230293249593).
+			Logger()
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().Msg(fakeMessage)
+			}
+		})
+	})
+}
+
+func BenchmarkLogFields(b *testing.B) {
+	b.Run("gologs", func(b *testing.B) {
+		logger := gologs.New(ioutil.Discard).SetInfo().SetTimeFormatter(gologs.TimeUnix)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().
+					String("string", "four!").
+					Int("int", 123).
+					Float("float", -2.203230293249593).
+					Msg(fakeMessage)
+			}
+		})
+	})
+
+	b.Run("zerolog", func(b *testing.B) {
+		logger := zerolog.New(ioutil.Discard)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info().
+					Str("string", "four!").
+					Time("time", time.Time{}).
+					Int("int", 123).
+					Float32("float", -2.203230293249593).
+					Msg(fakeMessage)
+			}
+		})
 	})
 }
